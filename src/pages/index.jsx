dc1cbd2a20a3f7c4d064d8f0f2566421cca115cd
holic5839 <holic5839@gmail.com>
2022-12-10 20:36:55 +0900

    feature: 카드 틀 작성

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const CardContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100vh;
+`;
+
+const CardWrapper = styled.div`
+    padding: 15px;
+    width: 300px;
+    height: 500px;
+    border: 1px solid #e5e5e5;
+    border-radius: 15px;
+    box-shadow: 20px 20px 20px 0 rgba(0, 0, 0, 0.1);
+`;
+
 const Main = () => {
     return (
-        <div>
-            <h1>프로젝트 준비완료</h1>
-        </div>
+        <CardContainer>
+            <CardWrapper>카드</CardWrapper>
+        </CardContainer>
     );
 };
 
