7e92be32ce2caf73f579a7bec27b70985c14257a
holic5839 <holic5839@gmail.com>
2022-12-10 20:27:47 +0900

    feature: 프로젝트 스캐폴딩

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import Main from './pages';
+import './css/index.css';
+
+const App = () => <Main />;
+
+export default App;
---
+++ src/pages/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Main = () => {
+    return (
+        <div>
+            <h1>프로젝트 준비완료</h1>
+        </div>
+    );
+};
+
+export default Main;
